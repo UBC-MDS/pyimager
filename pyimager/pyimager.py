f4630f119ffa7e5306db7e31ce86cb4e30b18122
Salama <dataubc@gmail.com>
2020-02-28 12:56:24 -0800

    added docstring for the new function reduce_dimensions

--- pyimager/pyimager.py
+++ pyimager/pyimager.py
@@ -34,3 +34,25 @@ def circropper(input_path, output_path):
     # Output image 
     Image.fromarray(imgArray).save(output_path)
 
+def reduce_dimensions(input_file, output_file, width, height):
+	     """  
+        A function to reduce the dimension of a give image by removing vertical and horizontal seams
+        
+        Parameters
+        ----------
+        input_file : path to the image to the input image
+        output_file : path to the image to the output image
+        newdim: new width the output image
+        height:new height of the output image
+        
+        Returns
+        -------
+        A new image with new width and height
+
+         Examples:
+        ---------
+        reduce_dimensions(bear.jpg", "result.png", 33, 33)
+        A file named "result.png"  with the width 33 and height 33 will be generated in the your
+         designated folder.
+        """
+
