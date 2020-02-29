671e67bd0edeb90211d0e6dd71f1c8cf515545b8
kknebel <keanna.knebel@gmail.com>
2020-02-28 19:54:07 -0800

    fixed formatting, spelling, and code errors in the two initial functions

--- pyimager/pyimager.py
+++ pyimager/pyimager.py
@@ -14,10 +14,9 @@ def circropper(input_path, output_path):
     
     Examples:
     ---------
-    >>> from pyimager import circropper
-    >>> circropper("bear.jpg", "result.png")
-    A file named "result.png" will be generated in the your
-    designated folder.
+    >>> from pyimager import pyimager
+    >>> pyimager.circropper("bear.jpg", "result.png")
+    A file named "result.png" will be generated in the current folder.
     """
     # Read in and convert image to np.array
     img=Image.open(input_path).convert("RGB")
@@ -35,24 +34,29 @@ def circropper(input_path, output_path):
     Image.fromarray(imgArray).save(output_path)
 
 def reduce_dimensions(input_file, output_file, width, height):
-	     """  
-        A function to reduce the dimension of a give image by removing vertical and horizontal seams
+	"""  
+    A function to reduce the dimension of a given image by removing vertical and horizontal seams
         
-        Parameters
-        ----------
-        input_file : path to the image to the input image
-        output_file : path to the image to the output image
-        newdim: new width the output image
-        height:new height of the output image
+    Parameters
+    ----------
+    input_file : str
+        path to the input image
+    output_file : str
+        path to the output image
+    width : int
+        new width the output image
+    height : int
+        new height of the output image
         
-        Returns
-        -------
-        A new image with new width and height
+    Returns
+    -------
+    A new image with new width and height
 
-         Examples:
-        ---------
-        reduce_dimensions(bear.jpg", "result.png", 33, 33)
-        A file named "result.png"  with the width 33 and height 33 will be generated in the your
-         designated folder.
-        """
+    Examples:
+    ---------
+    >>> from pyimager import pyimager
+    >>> pyimager.reduce_dimensions("bear.jpg", "result.png", 33, 33)
+    A file named "result.png"  with the width 33 and height 33 will be generated in the
+    current folder.
+    """
 
