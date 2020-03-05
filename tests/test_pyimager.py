1183f31b20ead63674135962482e78df33d9ff92
zhypan <apeggy123@hotmail.com>
2020-03-04 21:41:46 -0800

    update test and fix error

--- tests/test_pyimager.py
+++ tests/test_pyimager.py
@@ -1,15 +1,16 @@
 from pyimager.pyimager import *
 
-def test_stype():
+def test_reducolor():
     img_array = reducolor(0, 'tests/mandrill.jpg')
     color_n = len(np.unique(img_array.reshape(np.prod(img_array.shape[:2]),3), axis=0))
     assert color_n == 2, f'Should return two colors only, {color_n} colors are returned'
     img_array = reducolor(1, 'tests/mandrill.jpg')
     color_n = len(np.unique(img_array.reshape(np.prod(img_array.shape[:2]),3), axis=0))
-    assert color_n == 24, f'Should return eight colors, {color_n} colors are returned'    
+    assert color_n == 8, f'Should return eight colors, {color_n} colors are returned'
+    img_array = reducolor(1, 'tests/milad.jpg')
     try:
         img_array = reducolor(2, 'tests/mandrill.jpg')
-    except StyleException:
+    except AssertionError:
         pass
     else:
-        assert False, f'Should not allow code other than 0 and 1 and raise StyleException'
+        assert False, f'AssertionError should be raised. Should not allow code other than 0 and 1'
