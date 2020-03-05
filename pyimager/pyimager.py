a7a2d3a3155f1c2c8759c853797805ffb31a2893
Salama <dataubc@gmail.com>
2020-03-05 14:57:50 -0800

    added assesrtions for defensive prograaming

--- pyimager/pyimager.py
+++ pyimager/pyimager.py
@@ -1,9 +1,11 @@
 import numpy as np
 from PIL import Image, ImageDraw
+from matplotlib.image import imsave
+import matplotlib.pyplot as plt 
 
-def circropper(input_path, output_path):
+def circropper(input_path, output_path, margin):
     """
-    Crops an image into the circle shape.
+    Crops an image into a circle and leave some margin as you defined 
 
     Parameters:
     -----------
@@ -11,30 +13,42 @@ def circropper(input_path, output_path):
         The file path of the image to be cropped
     output_path: string
         The file path for cropped image 
-    
+    margin: float
+        The distance between circle boundary and the original image boundary
+
     Examples:
     ---------
-    >>> from pyimager import pyimager
-    >>> pyimager.circropper("bear.jpg", "result.png")
-    A file named "result.png" will be generated in the current folder.
+    >>> from pyimager import circropper
+    >>> circropper("bear.jpg", "result.png", 0)
+    A file named "result.png" will be generated in the 
+    designated folder.
     """
+
     # Read in and convert image to np.array
     img=Image.open(input_path).convert("RGB")
     imgArray=np.array(img)
-    height,weight=img.size
+    height,width=img.size
 
     # Create circle mask layer and crop 
     mask = Image.new('L', img.size,0)
     draw = ImageDraw.Draw(mask)
-    draw.pieslice([0,0,height,weight],0,360,fill=255)
+    draw.pieslice([margin,margin,height-margin,width-margin],0,360,fill=255)
     maskArray=np.array(mask)
     imgArray=np.dstack((imgArray,maskArray))
 
     # Output image 
     Image.fromarray(imgArray).save(output_path)
 
-def reduce_dimensions(input_file, output_file, width, height):
-	"""  
+
+
+
+import numpy as np
+from scipy.ndimage.filters import convolve
+import matplotlib.pyplot as plt
+
+
+def reduce_dimensions(input_file,output_file,new_width,new_height):
+    """  
     A function to reduce the dimension of a given image by removing vertical and horizontal seams
         
     Parameters
@@ -59,65 +73,57 @@ def reduce_dimensions(input_file, output_file, width, height):
     A file named "result.png"  with the width 33 and height 33 will be generated in the
     current folder.
     """
+    # reading the image's original dimension
+    image = plt.imread(input_file)
+    width = image.shape[1] 
+    height = image.shape[0]   
+    # asserting that the new dimensions are less than the original dimensions
+    if new_width>width:
+         raise AssertionError("New width should be less than the original width")
+    if new_height>height:
+        raise AssertionError("New height should be less than the original height")
+    # reducing the width dimension
+    for i in range(0,(width-new_width)):
+        dx = np.array([-1, 0, 1])[None, :, None]
+        dy = np.array([-1, 0, 1])[:, None, None]    
+        energy_img = convolve(image, dx)**2 + convolve(image, dy)**2
+        v_seam = np.zeros(energy_img.shape[0])
+        lin_inds = np.array(v_seam)+np.arange(image.shape[0])*image.shape[1]
+        new_image = np.zeros(
+            (height, image.shape[1]-1, image.shape[-1]), dtype=image.dtype) 
+        for j in range(image.shape[-1]):
+            temp = np.delete(image[:, :, j], lin_inds.astype(int))
+            temp = np.reshape(temp, (height, image.shape[1]-1))
+            new_image[:, :, j] = temp
+        image=new_image
+    width = image.shape[1] 
+    height = image.shape[0]
+     # reducing the height dimension
+    for i in range(0,(height-new_height)):
+        image= np.transpose(image, (1, 0, 2)) 
+        dx = np.array([-1, 0, 1])[None, :, None]
+        dy = np.array([-1, 0, 1])[:, None, None]    
+        energy_img = convolve(image, dx)**2 + convolve(image, dy)**2
 
-    import numpy as np
-    from scipy.ndimage.filters import convolve
-    import matplotlib.pyplot as plt
-    image = plt.imread("mandrill.jpg")
+        h_seam = np.zeros(energy_img.shape[0])
+        lin_inds = np.array(h_seam)+np.arange(image.shape[0])*image.shape[1]
+        new_image = np.zeros(
+            (width, image.shape[1]-1, image.shape[-1]), dtype=image.dtype) 
+        for c in range(image.shape[-1]):
+            temp = np.delete(image[:, :, c], lin_inds.astype(int))
+            temp = np.reshape(temp, (width, image.shape[1]-1))
+            new_image[:, :, c] = temp
+        image=np.transpose(new_image,(1, 0, 2))
+        
+    assert(image.shape[0] ==new_height)
+    assert(image.shape[1] ==new_width)
+        
+    plt.imsave(output_file, image)
+    return(image)
 
-    new_width = 200
-    new_height = 280
+    # examples :       
+    # python -c'import pyimager;pyimager.reduce_dimensions("../images/mandrill.jpg","../images/reduced_mandrill.jpg",210,200)'
 
-    def pyimager(input_file,output_file,new_width,new_height):
-        image = plt.imread(input_file)
-        width = image.shape[1] 
-        height = image.shape[0]
-        for i in range(0,(width-new_width)):
-
-            dx = np.array([-1, 0, 1])[None, :, None]
-            dy = np.array([-1, 0, 1])[:, None, None]    
-            energy_img = convolve(image, dx)**2 + convolve(image, dy)**2
-
-            v_seam = np.zeros(energy_img.shape[0])
-            lin_inds = np.array(v_seam)+np.arange(image.shape[0])*image.shape[1]
-            new_image = np.zeros(
-                (height, image.shape[1]-1, image.shape[-1]), dtype=image.dtype) 
-            for j in range(image.shape[-1]):
-                temp = np.delete(image[:, :, j], lin_inds.astype(int))
-                temp = np.reshape(temp, (height, image.shape[1]-1))
-                new_image[:, :, j] = temp
-            image=new_image
-
-
-        width = image.shape[1] 
-        height = image.shape[0]
-
-        for i in range(0,(height-new_height)):
-            image= np.transpose(image, (1, 0, 2)) 
-            dx = np.array([-1, 0, 1])[None, :, None]
-            dy = np.array([-1, 0, 1])[:, None, None]    
-            energy_img = convolve(image, dx)**2 + convolve(image, dy)**2
-
-            h_seam = np.zeros(energy_img.shape[0])
-            lin_inds = np.array(h_seam)+np.arange(image.shape[0])*image.shape[1]
-            new_image = np.zeros(
-                (width, image.shape[1]-1, image.shape[-1]), dtype=image.dtype) 
-            for c in range(image.shape[-1]):
-                temp = np.delete(image[:, :, c], lin_inds.astype(int))
-                temp = np.reshape(temp, (width, image.shape[1]-1))
-                new_image[:, :, c] = temp
-            image=np.transpose(new_image,(1, 0, 2))
-            
-        assert(image.shape[0] ==new_height)
-        assert(image.shape[1] ==new_width)
-            
-        plt.imsave(output_file, image)
-        return(image)
-
-    # examples : just for test will be deleted later      
-    r = pyimager("mandrill.jpg","images/reduced.jpg",210,200)
-    print(r.shape[0])
-    print(r.shape[1])
 
 
 def img_filter(input_path, filter_type, strength):
@@ -152,4 +158,60 @@ def img_filter(input_path, filter_type, strength):
     An array of pixels resulting in an image the same size as "bear.jpg" with a 
     moderate blured effect.
     """
+   
+class StyleException(Exception):
+    pass
+
+def reducolor(style, input_path, output_path=None):
+    '''
+    Reduce image colors
+
+    Parameters:
+    -----------
+    style: int, either 0 or 1
+        0 for white and black colors
+        1 for 8 colors
+    input_path: string
+        The file path of the image
+    output_path: string or None
+        if not None, the modified image will be saved
+        in the provided folder path and name
+
+    Returns:
+    --------
+    numpy.ndarray of the image and an image saved in the designated path 
     
+    Examples:
+    ---------
+    reducolor(0, 'tests/mandrill.jpg', 'tests/mandrill_new.jpg')
+    '''
+    
+    img = plt.imread(input_path)/255    
+
+    if style==0:  #black and white color
+        new_img = img.copy()
+        new_img[(img.mean(axis=2)<0.5), :] = np.array([0, 0, 0])  #less than gray
+        new_img[(img.mean(axis=2)>=0.5), :] = np.array([1, 1, 1])
+    
+    elif style==1:
+        red = img[:,:,0]
+        red[red<np.median(red)] = np.min(red)
+        red[red>np.median(red)] = np.max(red)
+        green = img[:,:,1]
+        green[green<np.median(green)] = np.min(green)
+        green[green>np.median(green)] = np.max(green)
+        blue = img[:,:,2]
+        blue[blue<np.median(blue)] = np.min(blue)
+        blue[blue>np.median(blue) ]= np.max(blue)
+        new_img = np.zeros(img.shape)
+        new_img[:, :, 0] = red
+        new_img[:, :, 1] = green
+        new_img[:, :, 2] = blue
+    else:
+        raise StyleException('Please enter valid style code.\n 0 for black and white color, 1 for eight color scales')
+    
+    if output_path is not None:
+       imsave(f'{output_path}', new_img)
+       print(f'New image saved in {output_path}')
+    
+    return new_img
