5750d79b24e7bfa8bc46981c2313876ae2ce480e
zoepan00 <54790610+zoepan00@users.noreply.github.com>
2020-03-04 20:09:02 -0800

    correct assertion color number
    Co-Authored-By: clsu22 <54614054+clsu22@users.noreply.github.com>

--- tests/test_pyimager.py
+++ tests/test_pyimager.py
@@ -6,7 +6,7 @@ def test_stype():
     assert color_n == 2, f'Should return two colors only, {color_n} colors are returned'
     img_array = reducolor(1, 'tests/mandrill.jpg')
     color_n = len(np.unique(img_array.reshape(np.prod(img_array.shape[:2]),3), axis=0))
-    assert color_n == 24, f'Should return eight colors, {color_n} colors are returned'    
+    assert color_n == 8, f'Should return eight colors, {color_n} colors are returned'    
     try:
         img_array = reducolor(2, 'tests/mandrill.jpg')
     except StyleException:
