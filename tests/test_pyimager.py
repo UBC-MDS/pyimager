fa93c83bd5ab642ff1d1634a70ca9a1563bec7b3
zhypan <apeggy123@hotmail.com>
2020-03-06 11:26:47 -0800

    moved images to images folder, update test for reducolor, cleanup code

--- tests/test_pyimager.py
+++ tests/test_pyimager.py
@@ -7,10 +7,11 @@ input_path = "./tests/milad.jpg"
 def test_circropper_output():
     img = Image.open(input_path).convert("RGB")
     imgArray_1 = np.array(img)
-    imgArray_2 = circropper(input_path, "result.png", 0)
+    imgArray_2 = circropper(input_path, "tests/result.png", 0)
     assert imgArray_1.shape[0] == imgArray_2.shape[0] 
     assert imgArray_1.shape[1] == imgArray_2.shape[1] 
     assert imgArray_1.shape[2] == imgArray_2.shape[2]-1
+    os.remove("tests/result.png")
 
 def test_circropper_input_value():
     try:
@@ -21,18 +22,35 @@ def test_circropper_input_value():
         print("not error happens")
 
 def test_reducolor():
+    '''
+    Unit test for the reducolor function
+    '''
     img_array = reducolor(0, 'tests/mandrill.jpg')
     color_n = len(np.unique(img_array.reshape(np.prod(img_array.shape[:2]),3), axis=0))
     assert color_n == 2, f'Should return two colors only, {color_n} colors are returned'
+    
     img_array = reducolor(1, 'tests/mandrill.jpg')
     color_n = len(np.unique(img_array.reshape(np.prod(img_array.shape[:2]),3), axis=0))
-    assert color_n == 8, f'Should return eight colors, {color_n} colors are returned'    
+    assert color_n == 8, f'Should return eight colors, {color_n} colors are returned'
+    
     try:
         img_array = reducolor(2, 'tests/mandrill.jpg')
     except AssertionError:
         pass
     else:
         assert False, f'AssertionError should be raised. Should not allow code other than 0 and 1'
+        
+    try:
+        img_array = reducolor(2, 'tests/wrong.jpg')
+    except FileNotFoundError:
+        pass
+    else:
+        assert False, f'FileNotFoundError should be raised for non-existant files.'
+        
+    reducolor(0, 'tests/mandrill.jpg', 'tests/mandrill_new.jpg')
+    assert os.path.exists('tests/mandrill_new.jpg'), 'File should be saved to the provided output path'
+    os.remove('tests/mandrill_new.jpg')
+        
 
 def test_reduce_dimensions(): 
     shape_even = reduce_dimensions("images/mandrill.jpg","images/reduced_mandrill.jpg",200,200).shape
